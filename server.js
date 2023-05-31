cb948b73f1003875c2120615b5b565f87cf86dd6
Mr Anonymous <azhansyakirin@gmail.com>
2023-05-31 20:04:00 +0800

    no need to depends on routes.js anymore ;)

--- server.js
+++ server.js
@@ -3,36 +3,56 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
-const { routes, pageRoutes } = require('./api/routes');
-
-// console.log({Route : routes, PageRoute : pageRoutes})
+//const { routes } = require('./api/routes');
 
 app.use(cors());
 
-routes.map(route => {
-    app.get(`/api/${route}`, (req, res) => {
-        try {
-            const jsonPath = path.join(__dirname, `api/${route}/index.json`);
-            const jsonData = require(jsonPath);
-            res.json(jsonData);
-        } catch (error) {
-            res.status(500).json({ error: { message: 'Unable to parse the response [200]' } });
-        }
-    });
+// Read the contents of the "api" folder
+const apiPath = path.join(__dirname, 'api');
+const apiFolders = fs.readdirSync(apiPath);
+
+// Dynamically create routes for each folder
+apiFolders.forEach(folder => {
+  const route = folder.toLowerCase();
+  const jsonPath = path.join(apiPath, folder, 'index.json');
+
+  app.get(`/api/${route}`, (req, res) => {
+    try {
+      const jsonData = require(jsonPath);
+      res.json(jsonData);
+    } catch (error) {
+      res.status(500).json({ error: { message: 'Unable to parse the response [200]' } });
+    }
+  });
 });
 
-pageRoutes.forEach(route => {
-    app.get(`/api/pages/${route}`, (req, res) => {
-        try {
-            const jsonPath = path.join(__dirname, `api/pages/${route}/index.json`);
-            const jsonData = require(jsonPath);
-            res.json(jsonData);
-        } catch (error) {
-            res.status(500).json({ error: { message: 'Unable to parse the response [200]' } });
-        }
-    });
+// Read the contents of the "api/pages" directory
+const pagePath = path.join(__dirname, 'api/pages');
+const pageFolders = fs.readdirSync(pagePath);
+
+// Dynamically create routes for each page folder
+pageFolders.forEach(folder => {
+  const route = folder.toLowerCase();
+  const jsonPath = path.join(pagePath, folder, 'index.json');
+
+  app.get(`/api/pages/${route}`, (req, res) => {
+    try {
+      const jsonData = require(jsonPath);
+      res.json(jsonData);
+    } catch (error) {
+      res.status(500).json({ error: { message: 'Unable to parse the response [200]' } });
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+console.log(`/////////////////////////////////////////////////`);
+console.log(`Server Directory`);
+console.log({pageFolders: pageFolders, apiFolders : apiFolders});
+console.log(`/////////////////////////////////////////////////`);
